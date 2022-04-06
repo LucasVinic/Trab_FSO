009215ac9444318418c20ee43aca5b00a45a4eb4
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-04-06 17:14:52 -0300

    test

--- CriaProcessos.c
+++ CriaProcessos.c
@@ -6,24 +6,26 @@
 #include <signal.h>
 #include <string.h>
 
-#define NUM_FILHOS 3
+#define NUM_FILHOS 10
 
 int children_not_yet_ready = NUM_FILHOS;
 int myPid;
 
 void recieved_ok_to_die(int signum) {
-  write(STDOUT_FILENO, "i did my thing. i can go in peace\n", 35);
 }
 
 void child_is_ready(int signum) {
-  write(STDOUT_FILENO, "one of my children told me they did their thing\n", 49);
   children_not_yet_ready--;
 }
 
 int main() {
   bool isParent = true;
-  int pids[NUM_FILHOS]; //lista de pids
+  int pids[NUM_FILHOS]; // lista de pids
   int parentPid = myPid = getpid();
+
+  // setup signal callbacks
+  signal(SIGUSR1, child_is_ready);
+  signal(SIGUSR2, recieved_ok_to_die);
 
   // fork processes
   for (int i = 0; i < NUM_FILHOS; i++) {
@@ -36,20 +38,19 @@ int main() {
     }
   }
 
-  // setup signal callbacks
-  signal(SIGUSR1, child_is_ready);
-  signal(SIGUSR2, recieved_ok_to_die);
-
   // só executa esse código se é filho
   if (!isParent) {
     myPid = getpid();
-    printf("hi, i'm process %d and i did my thing\n.", myPid);
-
+    
+    printf("hi, i'm process %d and i did my thing. \n", myPid);
+    
     // tell parent i did my thing
     kill(parentPid, SIGUSR1);
 
     // wait untill i have the ok to die
     pause();
+
+    printf("#%d recieved the ok to go.\n", myPid);
 
     // die
     exit(0);
@@ -58,10 +59,9 @@ int main() {
   // só executa esse código se é pai
   if (isParent) {
 
-    // wait for all children to run and give the ok signal
     while (children_not_yet_ready > 0) {
-      // printf("i have %d children not yet ready\n", children_not_yet_ready);
-      pause();
+      printf("i have %d children not yet ready\n", children_not_yet_ready);
+      sleep(2);
     }
 
     printf("i'm the parent, and all of my children did their things.\n");
@@ -76,9 +76,7 @@ int main() {
     // wait for all children to stop before exiting
     for (int i = 0; i < NUM_FILHOS; i++) {
       int status;
-      // printf("waiting for child #%d of pid %d.\n", i, pids[i]);
       waitpid(pids[i], &status, 0);
-      // printf("a total of %d of my children have died.\n", i+1);
     }
 
     // morre
