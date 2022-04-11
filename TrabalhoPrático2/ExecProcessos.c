2774efbbbaceb3b4ec9f30732e8b5f22d903ff77
MoustacheGolem <mateus_p_r@hotmail.com>
2022-04-10 21:42:06 -0300

    Trab. 2 pronto mas não revisado
    completo mas sem revisão

--- TrabalhoPrático2/ExecProcessos.c
+++ TrabalhoPrático2/ExecProcessos.c
@@ -7,59 +7,68 @@
 #include <string.h>
 #include <sys/msg.h>
 #include <time.h>
+#include <errno.h>
 
 #define NUM_FILHOS 5
-#define NUM_GRANDE 50000000000
+#define NUM_GRANDE 500000000
                  //50000000000
-#define RAND_CEILING 0
-#define RAND_FLOOR 0
+#define RAND_CEILING 10
+#define RAND_FLOOR 2
 
 int main () {
   time_t start_t, end_t;
   bool isParent = true;
   int tempo, myPid, i;
   int pids[NUM_FILHOS];
+  int tempos[NUM_FILHOS];
   
+
   srand(time(0));
 
   for(i = 0; i < NUM_FILHOS; i++){
     tempo = rand() % (RAND_CEILING - RAND_FLOOR + 1) + RAND_FLOOR;
     int temp = fork();
+    
     time(&start_t);
-   
     if (temp == 0) {
       isParent = false;
       break;
     } else {
-      printf("pid do filho i = %d é %d, tempo é %d\n", i, temp, tempo);
+      printf("pid do filho i = %d é %d, seu tempo_i é: %d\n", i, temp, tempo);
       pids[i] = temp;
+      tempos[i] = tempo; 
     }
   }
 
   if(!isParent) {
-    //imediatamente para a execução do filho
     
     sleep(tempo);
 
-    myPid = getpid();
-    for(long long j = 0; j < NUM_GRANDE; j++);
-    printf("meu pid é %d\n", myPid);
 
-    time(&end_t);
+    myPid = getpid();
     
+       
     
-    double turnaround_time = difftime(end_t, start_t);
-    printf("[%d] turnaround time: %lf\n", myPid, turnaround_time);
+    for(long long j = 0; j < NUM_GRANDE; j++);
+    printf("Meu pid é %d\n", myPid);
+
     exit(0);
   }
   
   if (isParent) {
-
+    
+    
     for (int i = 0; i < NUM_FILHOS; i++) {
       int tpid = wait(NULL);
-      //time(&end_t);
-      //ta = difftime(end_t, start_t);
-      //printf("Filho com pid %d, teve turnaround de %f",tpid,ta)
+      int lt = 0;
+      time(&end_t);
+      for(int j = 0;j < NUM_FILHOS;j++){
+          if(pids[j] == tpid){
+              lt = tempos[j];
+          }
+      }
+      double ta = difftime(end_t, start_t)-lt;
+      printf("Turnaround time do filho %d: %lf\n", tpid, ta);
     
     }
     return 0;
